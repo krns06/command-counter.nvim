3f397616f020b4004e9150137bbf8abe8382d5b7
krns06 <125727002+krns06@users.noreply.github.com>
2025-04-18 14:04:45 +0900

    fix key_logger

--- lua/command-counter.lua
+++ lua/command-counter.lua
@@ -4,16 +4,32 @@ M.config = {
         output_path = "/tmp/command_counter.nvim"
 }
 
-M.key_logger = function(key)
+M.key_logger = function(key, typed)
         local mode = vim.api.nvim_get_mode().mode
-        if mode == 'n' then
-                local file = io.open(M.config.output_path, "a")
-                if file then
-                        file:write("\"" .. key .. "\",")
-                        file:write(vim.fn.char2nr(key) .. ",")
-                        file:close()
+        local file = io.open(M.config.output_path, "a")
+        if file then
+                if mode ~= "i" then
+                        if key == ":" and typed ~= ":" then
+                                -- 内部的にコマンドとして実行される場合に実行される
+                                -- 想定としてはspace+fとかに設定しているときに内部的には:が実行され、typedにfが設定されるっぽい
+                                file:write(key .. typed)
+                        elseif typed ~= "" then
+                                -- 仕様？なのかわからないけどBSを打つと<80>kbに変換されてしまう。(80は16進数)
+                                -- 謎なので0x80で挟むようにする。
+
+                                local first = string.sub(key, 1, 1):byte()
+                                if first == 128 then
+                                        file:write(key .. string.char(128))
+                                else
+                                        file:write(key)
+                                end
+                        end
+                elseif key == string.char(27) then
+                        file:write(string.char(27))
                 end
         end
+
+        file:close()
 end
 
 M.setup = function(args)
